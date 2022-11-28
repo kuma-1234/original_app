b86e53118dec56e35c34fd4a09f4d4aa1cbc03fc
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-11-28 17:26:36 +0900

    ルートの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    password: 'users/password'
+  }
   # root 'homes#index'
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
