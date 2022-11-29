556f1c6c6c6db9c2c32fd0abd584ad738322fefe
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-11-29 11:01:30 +0900

    ✨ profileコントローラーの設定追加
    edit,update,showの設定

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,27 @@
 class ProfilesController < ApplicationController
+
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    if @profile.update(params_profile)
+      redirect_to profile_path(current_user), notice: 'プロフィールを編集しました'
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @profile = Profile.find(params[:id])
+  end
+
+  private 
+
+  def params_profile
+    params.require(:profile).permit(:prefecture, :drone_name, :main_crop, :introduction_year, :self_introduction)
+  end
+
+
 end
