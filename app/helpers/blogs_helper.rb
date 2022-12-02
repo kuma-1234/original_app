f620e8de988fc5275c51ae2b51f5f4f27ecf141e
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-12-02 18:28:25 +0900

    ✨ placeholderの改行の設定

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,9 @@
 module BlogsHelper
+  def blog_content 
+    <<-"EOS".strip_heredoc
+      散布で気づいたこと気になったことを入力しましょう！
+      (例)初めて1キロ粒剤を散布した。説明書通りに散布したが剤が落ちすぎた。
+      速度を少しあげて作業したらちょうど良く散布できた。
+    EOS
+  end
 end
