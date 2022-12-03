889802990b5ed6838b00fb97884d72fd3d408001
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-12-03 22:49:22 +0900

    ➕ user/blogのアソシエーション
    アソシエーション以外にファイル名の修正も実施

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_03_123904) do
+ActiveRecord::Schema.define(version: 2022_12_03_134124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2022_12_03_123904) do
     t.string "spray_area"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "drones", force: :cascade do |t|
@@ -78,6 +80,7 @@ ActiveRecord::Schema.define(version: 2022_12_03_123904) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "blogs", "users"
   add_foreign_key "drones", "profiles"
   add_foreign_key "match_drones", "blogs"
   add_foreign_key "match_drones", "drones"
