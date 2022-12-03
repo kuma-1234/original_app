811d15a7c15dea229436d00195f6b26ff8668b1d
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-12-03 22:52:39 +0900

    ⚡️ 投稿にユーザー名とアイコンが表示されるように

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,7 +9,7 @@ class BlogsController < ApplicationController
   end
 
   def create
-    @blog = Blog.new(blog_params)
+    @blog = current_user.blogs.build(blog_params)
     if @blog.save
       redirect_to blogs_path, notice: '新規作成しました！'
     else
