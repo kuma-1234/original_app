e5f44200c5156b9f74f4aaf7e4009229c9ebde90
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-12-05 16:50:30 +0900

    ✨ コメント投稿機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,21 @@
 class CommentsController < ApplicationController
+
   def create
+    @blog = Blog.find(params[:blog_id])
+    @comment = @blog.comments.build(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to blog_path(@blog) }
+      else
+        format.html { redirect_to blog_path(@blog) }
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:talk)
   end
 end
