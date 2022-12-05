56b336e781e33ce4f6391e3a6cc07125399786c0
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-12-05 17:12:22 +0900

    ⚡️ コメント削除機能の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,15 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      flash.now[:notice] = 'コメントが削除されました'
+      format.js { render :index }
+    end
+  end
+
   private
 
   def comment_params
