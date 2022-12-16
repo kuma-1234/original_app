70f918cf93ab66115d33865b9f2f4bb076bc5012
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-12-16 17:24:54 +0900

    ⚡️ ゲスト用アカウントの削除ができないように実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :ensure_normal_user, only: :destroy
+
+  def ensure_normal_user
+    if resource.email == 'guest@example.com' || resource.email == 'guest_admin@example.com'
+      redirect_to profile_path(current_user), alert: 'ゲストユーザーは削除できません。'
+    end
+  end
 
   # GET /resource/sign_up
   def new
@@ -11,58 +16,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @profile = @user.build_profile
   end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
   protected
   # アカウント編集後、プロフィール画面に移動する
   def after_update_path_for(resource)
     profile_path(current_user)
   end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
+  #ログイン後にマイページへ
   def after_sign_up_path_for(resource)
     profile_path(resource)
   end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
