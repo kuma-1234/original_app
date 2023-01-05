89f69cc6887b30141c4467656aa21e9c2f91f8b7
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2023-01-05 11:07:41 +0900

    add: DM機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_26_042424) do
+ActiveRecord::Schema.define(version: 2022_12_27_082434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,16 @@ ActiveRecord::Schema.define(version: 2022_12_26_042424) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id", "recipient_id"], name: "index_conversations_on_sender_id_and_recipient_id", unique: true
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
+  end
+
   create_table "drones", force: :cascade do |t|
     t.string "drone_name"
     t.datetime "created_at", precision: 6, null: false
@@ -73,6 +83,17 @@ ActiveRecord::Schema.define(version: 2022_12_26_042424) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["blog_id"], name: "index_match_drones_on_blog_id"
     t.index ["drone_id"], name: "index_match_drones_on_drone_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "conversation_id", null: false
+    t.bigint "user_id", null: false
+    t.boolean "read"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -123,5 +144,7 @@ ActiveRecord::Schema.define(version: 2022_12_26_042424) do
   add_foreign_key "favorites", "users"
   add_foreign_key "match_drones", "blogs"
   add_foreign_key "match_drones", "drones"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
   add_foreign_key "profiles", "users"
 end
